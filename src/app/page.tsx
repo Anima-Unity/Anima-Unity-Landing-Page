f041d366db415108669847b36ecc1c6caebf00de
A-fauzi <akhmadfauzi0101@gmail.com>
2025-04-21 20:40:52 +0700

    hide pricing section in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Hero />
       <Features />
       <About />
-      <Pricing />
+      {/*<Pricing />*/}
       <Testimonials />
       <HowItWorks />
       <PartnersSection />
